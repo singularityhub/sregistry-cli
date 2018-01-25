a179ecb6bbcac4dcc29e8f06f595069650e91e25
vsoch <vsochat@stanford.edu>
2018-01-25 15:28:19 -0800

    adding dummy add

--- sregistry/main/__init__.py
+++ sregistry/main/__init__.py
@@ -100,6 +100,11 @@ def get_client(image=None):
         Client.get_container = get_container
         Client.get_collection = get_collection
 
+    # If no database, import dummy functions that return the equivalent
+    else:
+        from sregistry.database.dummy import ( add )
+        Client.add = add
+
     # Initialize the database
     cli = Client()
 
