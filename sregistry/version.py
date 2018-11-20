af2da56965395c0ea3a4cdada7e31496e78e7bc9
Vanessa Sochat <vsochat@stanford.edu>
2018-11-20 10:55:25 -0500

    bumping version to 1.0.02

--- sregistry/version.py
+++ sregistry/version.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
 
-__version__ = "0.01.01"
+__version__ = "0.01.02"
 AUTHOR = 'Vanessa Sochat'
 AUTHOR_EMAIL = 'vsochat@stanford.edu'
 NAME = 'sregistry'
