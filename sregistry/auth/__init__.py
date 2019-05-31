f8c948a0da4a8168146f754dfeb33e5e0457054d
Vanessa Sochat <vsochat@stanford.edu>
2019-05-31 12:30:18 -0400

    working on linting
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- sregistry/auth/__init__.py
+++ sregistry/auth/__init__.py
@@ -1,4 +1,14 @@
-from .utils import *
+'''
+
+Copyright (C) 2017-2019 Vanessa Sochat.
+
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+'''
+
+from .utils import basic_auth_header
 from .secrets import (
     get_secrets_file,
     get_credential_cache,
